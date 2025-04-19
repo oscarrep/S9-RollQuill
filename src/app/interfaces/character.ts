9792573f8e3ddc086eb26661da8d6ac43d4465d3
oscarrep <oscar.bcn.95@gmail.com>
2025-04-19 22:55:08 +0200

    add character interface + character service

---
+++ src/app/interfaces/character.ts
@@ -0,0 +1,12 @@
+export interface Character {
+    createdBy: string ,
+    name: string,
+    race: string,
+    subrace: string,
+    class: string,
+    subclass: string,
+    level: number,
+    stats: number[],
+    skills: string[],
+    expertise: string[],
+}
---
+++ src/app/services/character.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Character } from '../interfaces/character';
+import { Observable } from 'rxjs';
+import  {environment}  from '../../environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CharacterService {
+  private appUrl: string;
+  private apiUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.appUrl = environment.host;
+    this.apiUrl = environment.apiCharacters;
+  }
+
+  getCharacterList(): Observable<Character[]> {
+    return this.http.get<Character[]>(`${this.appUrl}${this.apiUrl}`);
+  }
+
+  deleteCharacter(id: number | undefined): Observable<void> {
+    return this.http.delete<void>(`${this.appUrl}${this.apiUrl}${id}`)
+  }
+
+  saveCharacter(character: Character): Observable<void> {
+    return this.http.post<void>(`${this.appUrl}${this.apiUrl}`, character)
+  }
+
+  getCharacter(id: number | undefined): Observable<Character> {
+    return this.http.get<Character>(`${this.appUrl}${this.apiUrl}${id}`)
+  }
+
+  updateCharacter(id: number | undefined, character: Character): Observable<void> {
+    return this.http.put<void>(`${this.appUrl}${this.apiUrl}${id}`, character)
+  }
+}
