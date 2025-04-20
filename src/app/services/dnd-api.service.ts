fae35c9fbc4b3f2bac0b3c84e3bed668cbe663e8
oscarrep <oscar.bcn.95@gmail.com>
2025-04-21 01:52:01 +0200

    add methods to get the character's race and class info

--- src/app/services/dnd-api.service.ts
+++ src/app/services/dnd-api.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DndApiService {
+  private hostUrl: string;
+  private races: string;
+  private classes: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.hostUrl = environment.dndHost;
+    this.races = environment.dndRaces;
+    this.classes = environment.dndClasses;
+  }
+
+  getRaces():Observable<any>{
+    return this.http.get<any[]>(`${this.hostUrl}${this.races}`);
+  }
+
+  getRaceInfo(race:string|undefined):Observable<any>{
+    return this.http.get<any[]>(`${this.hostUrl}${this.races}/${race}`);
+  }
+
+  getClassInfo(_class:string|undefined):Observable<any>{
+    return this.http.get<any[]>(`${this.hostUrl}${this.classes}/${_class}`);
+  }
 }
