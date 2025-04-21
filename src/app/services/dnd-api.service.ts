b6e7fea2b737842d8355f85ef438d49ae371104d
oscarrep <oscar.bcn.95@gmail.com>
2025-04-21 12:34:55 +0200

    add method to get class level info

--- src/app/services/dnd-api.service.ts
+++ src/app/services/dnd-api.service.ts
@@ -11,11 +11,13 @@ export class DndApiService {
   private hostUrl: string;
   private races: string;
   private classes: string;
+  private levels: string;
 
   constructor(private http: HttpClient) {
     this.hostUrl = environment.dndHost;
     this.races = environment.dndRaces;
     this.classes = environment.dndClasses;
+    this.levels = environment.dndLevels;
   }
 
   getRaces():Observable<any>{
@@ -28,5 +30,8 @@ export class DndApiService {
 
   getClassInfo(_class:string|undefined):Observable<any>{
     return this.http.get<any[]>(`${this.hostUrl}${this.classes}/${_class}`);
+  }
+  getClassLevelInfo(_class:string|undefined, level:number|undefined):Observable<any>{
+    return this.http.get<any[]>(`${this.hostUrl}${this.classes}/${_class}${this.levels}/${level}`);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,5 +11,6 @@ export const environment = {
   dndClasses: '',
   dndSubclasses: '',
   dndFeatures: '',
+  dndLevels: '',
 }
 
