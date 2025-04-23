959d05cafdea624af770457ef228ede6fe15385c
oscarrep <oscar.bcn.95@gmail.com>
2025-04-23 10:41:16 +0200

    add user to mongodb on registration

--- src/app/components/auth/signup/signup.component.ts
+++ src/app/components/auth/signup/signup.component.ts
@@ -5,6 +5,7 @@ import { Location } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from "../../button/button.component";
 import { SessionService } from '../../../services/session.service';
+import { environment } from '../../../../environments/environment';
 
 
 @Component({
@@ -20,12 +21,28 @@ export class SignupComponent {
   private router = inject(Router);
   private location = inject(Location)
   private _sessionService = inject(SessionService);
+  appUrl = environment.host;
+  appUsers = environment.apiUsers;
 
   signUp() {
     createUserWithEmailAndPassword(this.fireAuth, this.email, this.password)
       .then(userCredentials => {
-        console.log('User created:', userCredentials.user.uid, userCredentials.user.email, this.password)
+        const user = userCredentials.user
+
+        console.log('User created:', user.uid, user.email, this.password)
+
+        fetch(`${this.appUrl}${this.appUsers}`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ fireUid: userCredentials.user.uid, email: user.email})
+        })
+          .then(res => res.json())
+          .then(data => console.log(data))
+          .catch(err => console.error('DB save failed', err));
+
         this._sessionService.setSession(true);
+
+        if (this._sessionService.getSession() === true) this.router.navigate(['/dashboard']);
       })
       .catch(error => console.error('Error creating user:', error));
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: false,
   host: '',
   apiCharacters: '',
+  apiUsers: '',
 
   dndHost: '',
   dndRaces: '',
