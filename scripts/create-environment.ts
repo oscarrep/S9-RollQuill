1127c571fa2cd4af64d24fcff4a2461f3a70184c
oscarrep <oscar.bcn.95@gmail.com>
2025-04-24 03:01:47 +0200

    add create-environment.ts

---
+++ scripts/create-environment.ts
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+
+const targetPath = path.join(__dirname, 'src/environments/environment.prod.ts');
+
+const envConfig = `
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "${process.env['NG_APP_FIREBASE_API_KEY']}",
+    authDomain: "${process.env['NG_APP_FIREBASE_AUTH_DOMAIN']}",
+    projectId: "${process.env['NG_APP_FIREBASE_PROJECT_ID']}",
+    storageBucket: "${process.env['NG_APP_FIREBASE_STORAGE_BUCKET']}",
+    messagingSenderId: "${process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID']}",
+    appId: "${process.env['NG_APP_FIREBASE_APP_ID']}",
+  }
+};`;
+
+fs.writeFileSync(targetPath, envConfig);
+console.log('environment.prod generated')
