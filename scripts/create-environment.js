3a60a11b61806b3b2e014444ee6bc5aa9cb756e9
oscarrep <oscar.bcn.95@gmail.com>
2025-04-25 10:54:08 +0200

    update create env

--- scripts/create-environment.js
+++ scripts/create-environment.js
@@ -10,6 +10,19 @@ const targetPath = join(__dirname, '..', 'src/environments/environment.prod.ts')
 const envConfig = `
 export const environment = {
   production: true,
+
+  host: '${process.env['host']}',
+  apiCharacters: '${process.env['apiCharacters']}',
+  apiUsers: '${process.env['apiUsers']}',
+
+  dndHost: '${process.env['dndHost']}',
+  dndRaces: '${process.env['dndRaces']}',
+  dndSubraces: '${process.env['dndSubraces']}',
+  dndClasses: '${process.env['dndClasses']}',
+  dndSubclasses: '${process.env['dndSubclasses']}',
+  dndFeatures: '${process.env['dndFeatures']}',
+  dndLevels: '${process.env['dndLevels']}',
+
   firebaseConfig: {
     apiKey: "${process.env['NG_APP_FIREBASE_API_KEY']}",
     authDomain: "${process.env['NG_APP_FIREBASE_AUTH_DOMAIN']}",
