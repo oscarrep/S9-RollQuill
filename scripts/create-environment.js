5bf7237ca2a403e96f644030e8e0d36734627dd9
oscarrep <oscar.bcn.95@gmail.com>
2025-04-25 03:32:02 +0200

    fix: formatting of appId

--- scripts/create-environment.js
+++ scripts/create-environment.js
@@ -16,7 +16,7 @@ export const environment = {
     projectId: "${process.env['NG_APP_FIREBASE_PROJECT_ID']}",
     storageBucket: "${process.env['NG_APP_FIREBASE_STORAGE_BUCKET']}",
     messagingSenderId: "${process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID']}",
-    appId: "${process.env['NG_APP_FIREBASE_APP_ID']}",
+    appId: "${process.env['NG_APP_FIREBASE_APP_ID']}"
   }
 };`;
 
