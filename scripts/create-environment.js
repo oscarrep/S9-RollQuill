37ee2ed5cdab00547a820f88009317288d908aaa
oscarrep <oscar.bcn.95@gmail.com>
2025-04-25 03:18:45 +0200

    update create env

--- scripts/create-environment.js
+++ scripts/create-environment.js
@@ -1,6 +1,6 @@
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-import { fs } from 'fs';
+import { writeFileSync } from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
