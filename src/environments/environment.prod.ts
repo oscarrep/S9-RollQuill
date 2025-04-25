7f5025a62b76e369ae8de70c46acc6821bfdaeaa
oscarrep <oscar.bcn.95@gmail.com>
2025-04-25 10:17:14 +0200

    add empty env.prod.ts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,24 @@
+export const environment = {
+  production: true,
+  host: '',
+  apiCharacters: '',
+  apiUsers: '',
+
+  dndHost: '',
+  dndRaces: '',
+  dndSubraces: '',
+  dndClasses: '',
+  dndSubclasses: '',
+  dndFeatures: '',
+  dndLevels: '',
+
+  firebaseConfig: {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  }
+}
+
