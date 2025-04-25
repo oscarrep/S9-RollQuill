5af26efd5f33355204e5af3a6fd7f4356bf47ddb
oscarrep <oscar.bcn.95@gmail.com>
2025-04-25 03:16:32 +0200

    update create env

--- scripts/create-environment.js
+++ scripts/create-environment.js
@@ -1,10 +1,11 @@
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import { fs } from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const targetPath = join(__dirname,'..', 'src/environments/environment.prod.ts');
+const targetPath = join(__dirname, '..', 'src/environments/environment.prod.ts');
 
 const envConfig = `
 export const environment = {
