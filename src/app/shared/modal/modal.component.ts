3cf17cc7e332c16941fc0b22a97647be8b3c27e3
oscarrep <oscar.bcn.95@gmail.com>
2025-05-05 17:46:10 +0200

    hp-modal passes tests

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,11 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { HpEditModalComponent } from "./hp-modal/hp-modal.component";
+import { HpModalComponent } from "./hp-modal/hp-modal.component";
 import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-modal',
   standalone:true,
-  imports: [HpEditModalComponent, ButtonComponent],
+  imports: [HpModalComponent, ButtonComponent],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss'
 })
