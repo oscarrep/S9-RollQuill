d6de246d053f23a35f49e889b50e8f9713eb06f4
oscarrep <oscar.bcn.95@gmail.com>
2025-05-06 01:32:15 +0200

    Upload image to cloudinary

--- src/app/shared/modal/img-modal/img-modal.component.ts
+++ src/app/shared/modal/img-modal/img-modal.component.ts
@@ -1,9 +1,13 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { InputComponent } from "../../input/input.component";
+import { ButtonComponent } from "../../button/button.component";
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-img-modal',
-  imports: [InputComponent],
+  standalone: true,
+  imports: [InputComponent, ButtonComponent],
   templateUrl: './img-modal.component.html',
   styleUrl: './img-modal.component.scss'
 })
@@ -11,4 +15,48 @@ export class ImgModalComponent {
   @Input() characterId!: string;
   @Output() updated = new EventEmitter<number>();
   @Output() onClose = new EventEmitter<void>();
+  http = inject(HttpClient);
+  imgUrl: string = '';
+  file = null;
+
+
+  loadCloudinaryWidget(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      if ((window as any).cloudinary) {
+        resolve();
+        return;
+      }
+
+      const script = document.createElement('script');
+      script.src = environment.cloudinaryWidget;
+      script.onload = () => resolve();
+      script.onerror = () => reject(new Error('Cloudinary script failed to load'));
+      document.body.appendChild(script);
+    });
+  }
+
+  async openUploadWidget() {
+    await this.loadCloudinaryWidget();
+
+    const cloudinary = (window as any).cloudinary;
+    const widget = cloudinary.createUploadWidget({
+      cloudName: environment.cloudinaryCloudName,
+      uploadPreset: environment.cloudinaryPreset,
+    }, (error: any, result: any) => {
+      if (!error && result.event === 'success') {
+        console.log('Uploaded image URL:', result.info.secure_url);
+
+      }
+    });
+
+    widget.open();
+  }
+
+  apply(): void {
+
+  }
+
+  close(): void {
+    this.onClose.emit();
+  }
 }
