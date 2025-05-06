fb8fae40567bbc33d98c9088001b37b28c606331
oscarrep <oscar.bcn.95@gmail.com>
2025-05-06 04:15:40 +0200

    add character footer component, fixed header

---
+++ src/app/components/sections/character-footer/character-footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-character-footer',
+  imports: [],
+  templateUrl: './character-footer.component.html',
+  styleUrl: './character-footer.component.scss'
+})
+export class CharacterFooterComponent {
+
+}
