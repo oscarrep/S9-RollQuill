a113c0fd88f1a6c8409cd19934c6cc8fccac42e4
oscarrep <oscar.bcn.95@gmail.com>
2025-05-07 01:32:48 +0200

    updated auth guard

--- src/app/guards/auth-guard.guard.ts
+++ src/app/guards/auth-guard.guard.ts
@@ -2,14 +2,28 @@ import { CanActivateFn, Router, RouterStateSnapshot, ActivatedRouteSnapshot } fr
 import { inject } from '@angular/core';
 import { SessionService } from '../services/session.service';
 
-
 export const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
   const router: Router = inject(Router);
   const sessionService = inject(SessionService);
 
-  if (!sessionService.getSession() && state.url.startsWith('/starships')) {
+  const session = sessionService.getSession();
+  const user = sessionService.getUser();
+  const uid = route.params['uid'];
+
+  if (!session || !user) {
     router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
     return false;
   }
-  else return true;
+
+  if (user._id !== uid) {
+    router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+    return false  }
+
+  if (state.url.includes('/create') && !user.premium && user.characters?.length >= 2) {
+    alert('You need premium to create more than 2 characters.');
+    router.navigate([`/${user._id}/dashboard`]);
+    return false;
+  }
+
+  return true;
 };
